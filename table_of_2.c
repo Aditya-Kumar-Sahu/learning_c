c92ca138f94a2b766686e891cf593c987f2b109c
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-07-22 20:01:01 +0530

    code correction of table of 2

--- table_of_2.c
+++ table_of_2.c
@@ -3,12 +3,12 @@
 int main()
 {
     int i=1;
-    printf("Table of 2:-\n");
+    printf("Table of 2 is:-\n");
     do
     {
         printf("%d\n", i*2);
         i++;
     }
-    while(i<=9);
+    while(i<=10);
     return 0;
 }
