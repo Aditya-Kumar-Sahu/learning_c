24c7bde2f4132ba35e9e97c129b38bc0d816add6
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-08-02 21:43:44 +0530

    adding documentation in pyramid code

--- pyramid.c
+++ pyramid.c
@@ -1,16 +1,23 @@
+/*
+Program to print given pyramid pattern
+      *
+    * * * 
+  * * * * *
+* * * * * * *
+*/
 #include <stdio.h>
 int main()
 {
     int i,j,k,n;
     printf("Enter height of the pyramid: ");
     scanf("%d", &n);
-    for (i=0; i<=n; i++)
+    for (i=0; i<=n; i++)            //number of rows, or, height of the pyramid
     {
-        for(j=0; j<=2*(n-i); j++)
+        for(j=0; j<=2*(n-i); j++)   //number of white space columns
         {
             printf(" ");
         }
-        for (k=0; k<((2*i)-1); k++)
+        for (k=0; k<((2*i)-1); k++) //number of *
         {
             printf("* ");
         }
